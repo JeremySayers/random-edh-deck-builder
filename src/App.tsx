d361e31f7aac9009d64d5293f6bd294d7a993261
Jeremy Sayers <jeremy1080@gmail.com>
2020-11-18 16:32:20 -0500

    basic search working, not the best, will improve soonTM

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import CardSelectionComponent from './components/card-selection-component';
 import { ProgressSpinner } from 'primereact/progressspinner';
 import ConfirmationDialogComponent from './components/confirmation-dialog-component';
 import ExportService from './services/export-service';
+import { InputText } from 'primereact/inputtext';
+
 /**
  * Interface used for the current state of loading.
  */
@@ -25,6 +27,7 @@ const App: React.FunctionComponent = () => {
     const[currentDeck, setCurrentDeck] = useState<Card[]>([]);
     const[loadingStatus, setLoadingStatus] = useState<loadingStatusProps>({complete: false});
     const[showConfirmationDialog, setShowConfirmationDialog] = useState<boolean>(false);
+    const[searchString, setSearchString] = useState<string>("");
 
     const scrollRef = useRef<HTMLInputElement>(null);
 
@@ -56,6 +59,7 @@ const App: React.FunctionComponent = () => {
         setShowConfirmationDialog(false);        
         if (confirm) {
             setCurrentDeck([]);
+            setSearchString("");
             setCurrentlyShowingTypes(CardType.AllTypes);
         }
     }
@@ -80,7 +84,7 @@ const App: React.FunctionComponent = () => {
             return;
         }
 
-        let cards = commanderOnly ? cardCache.filter((card: Card) => {return card.canBeCommander && card.type_line.includes('Eldrazi')}) : cardCache;
+        let cards = commanderOnly ? cardCache.filter((card: Card) => {return card.canBeCommander}) : cardCache;
 
         if (!commanderOnly && currentDeck.length > 0) {
             cards = cards.filter((card) => {
@@ -94,6 +98,50 @@ const App: React.FunctionComponent = () => {
             if (currentlyShowingTypes.searchString !== undefined) {
                 cards = cards.filter((card) => {
                     return card.type_line.toLowerCase().includes(currentlyShowingTypes.searchString!);
+                });
+            }
+        }
+
+        // if we have a search string, let's parse it and filter based on it
+        if (searchString && searchString.trim() !== "") {
+            let terms = searchString.toLowerCase().split(" ");
+
+            let usedTerms: string[] = [];
+            
+            terms.map((term: string, index: number) => {
+                if (['red', 'blue', 'black', 'white', 'green'].includes(term)) {
+                    let colorCode = term[0].toUpperCase();
+                    if (colorCode === 'B'){
+                        colorCode = 'U'
+                    }
+                    
+                    usedTerms.push(term);
+                    cards = cards.filter((card) => {
+                        return card.colors?.includes(colorCode) && card.colors?.length === 1;
+                    });
+                }
+
+                if (['instant', 'artifact', 'enchantment', 'creature', 'sorcery', 'land', 'planeswalker'].includes(term)) {
+                    usedTerms.push(term);
+                    cards = cards.filter((card) => {
+                        return card.type_line.toLowerCase().includes(term.toLowerCase());
+                    });
+                }
+            });
+
+            // remove the used terms
+            usedTerms.map((term: string) =>{
+                const index = terms.indexOf(term);
+                if (index !== -1) {
+                    terms.splice(index);
+                }
+            });
+
+            if (terms.length > 0) {
+                let newSearchString = terms.join(" ");
+
+                cards = cards.filter((card) => {
+                    return card.name.toLowerCase().includes(newSearchString.toLowerCase());
                 });
             }
         }
@@ -128,7 +176,7 @@ const App: React.FunctionComponent = () => {
 
         setCards(newCards);
         scrollRef.current?.scrollIntoView({behavior: "smooth"});
-    }, [cardCache, currentDeck, currentlyShowingTypes]);
+    }, [cardCache, currentDeck, currentlyShowingTypes, searchString]);
 
     /**
      * Called on initial load, fetches the card database from the server.
@@ -167,6 +215,10 @@ const App: React.FunctionComponent = () => {
             {cardCache?.length > 0
             ?
                 <div>
+                    <span className="p-input-icon-left">
+                        <i className="pi pi-search" />
+                        <InputText value={searchString} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchString(e.target.value)} placeholder="Search" />
+                    </span>
                     <CardSelectionComponent
                         currentDeck={currentDeck} 
                         handleDontLikeTheseClick={handleDontLikeTheseClick} 
